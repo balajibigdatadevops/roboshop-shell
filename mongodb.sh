1d9e0bbf619551244c9186878e84cb9c08befe10
gbalaji <gadi.balaji@epsilon.com>
2023-12-23 05:37:34 +0530

    pushing scripts

--- mongodb.sh
+++ mongodb.sh
@@ -29,7 +29,7 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 VALIDATE $? "copying MONGO repo file..."
 
