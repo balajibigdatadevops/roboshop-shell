db5d6ea0a8b009df6ead5c36d9bb67022907be51
gbalaji <gadi.balaji@epsilon.com>
2023-12-24 16:57:43 +0530

    pushing scripts

--- redis.sh
+++ redis.sh
@@ -43,7 +43,7 @@ dnf install redis -y &>> $LOGFILE
 
 VALIDATE $? "Installing redis"
 
-sed -i 's/127.0.0.1/0.0.0.0' /etc/redis.conf &>> $LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>> $LOGFILE
 
 VALIDATE $? "allowing remote connections"
 
